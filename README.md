f3eebec34cf4c0d78e1f03d81cb1408af85a80f2
Manuel Castaneda <Ma52Castaneda@gmail.com>
2019-08-09 22:43:36 -0700

    Made minor changes to app.py and wrote a breif readme

---
+++ README.md
@@ -0,0 +1,23 @@
+#Project Creator
+A Yython automation app that gets you started with your project. For windows. Not sure how this will work on Mac.
+
+##Instalation
+You will need to install selenium
+
+'''bash
+pip install -U selenium
+'''
+
+And also install Chrome Driver. Then put chrome driver in a folder, copy the path to that folder, and add that path as the value of the environmental system variable 'path'
+
+##Usage
+Open up app.py and enter your github username and password on lines 8 and 9.
+
+Change 11 line to the path where you want to create your new folder
+
+To create a project, open your terminal and change dir into this folder. Then, run the python script along with your project's name
+
+'''bash
+python app.py myProject
+'''
+
--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import os
 import subprocess
 import sys
-import selenium
 from selenium import webdriver
 from time import sleep
 
+#github creds
+github_Username = "Lucidreline"
+github_Password = os.environ.get("GITHUB_PW") #Put your password here
 
+projectsFolderPath = "C:/Users/ma52c/Storage/Coding/Projects/"
 
+browser = webdriver.Chrome()
 
 
 #Takes in argument from the terminal and setsit as the project name
@@ -19,23 +23,14 @@ for file in filesInDir:
         print("ERROR: Project name already exsists. Pick a different name!")
         quit()
 
-github_Username = "Lucidreline"
-github_Password = os.environ.get("GITHUB_PW")
-
-
-chrome_options = webdriver.ChromeOptions()
-prefs = {"profile.default_content_setting_values.notifications" : 2}
-chrome_options.add_experimental_option("prefs",prefs)
-browser = webdriver.Chrome(r"C:\Users\ma52c\Storage\Coding\Projects\ProjectCreater\chromedriver.exe")
-
 #changes to the projects dir
-os.chdir("C:/Users/ma52c/Storage/Coding/Projects/")
+os.chdir(projectsFolderPath)
 
 #creates folder
 os.system("mkdir " + projectName)
 
 #go into the new project folder
-os.chdir("C:/Users/ma52c/Storage/Coding/Projects/" + projectName + "/")
+os.chdir(projectsFolderPath + projectName + "/")
 
 #creates the files
 filesToCreate = ["app.py", "README.md", "CheckList.txt"]
@@ -43,12 +38,6 @@ for file in filesToCreate:
     os.system("type nul > " + file)
 
 
-
-
-
-
-
-    
 def gitSetup(_projectName):
     
 
@@ -77,15 +66,21 @@ def gitSetup(_projectName):
     repoLink = browser.find_elements_by_xpath("//*[@id='empty-setup-push-repo-echo']/span[1]/span")[0].text
     repoLink = str(repoLink)
 
-    #close browser
-    browser.close()
-
     #basic commands to init the local repo and connect it to the remote one
     gitCommands = ["git init", "git add .", """git commit -m "first commit" """, "git remote add origin " + repoLink, "git push -u origin master"]
     
     for command in gitCommands:
         os.system(command)
+    
         
+    #make github repo private 
+    browser.get("https://github.com/" + github_Username + "/" + projectName + "/settings")
+    browser.find_elements_by_xpath("//*[@id='options_bucket']/div[8]/ul/li[1]/details/summary")[0].click()
+    browser.find_elements_by_xpath("//*[@id='options_bucket']/div[8]/ul/li[1]/details/details-dialog/div[4]/form/p/input")[0].send_keys(projectName)
+    browser.find_elements_by_xpath("//*[@id='options_bucket']/div[8]/ul/li[1]/details/details-dialog/div[4]/form/div/button")[0].click()
+
+    #close browser
+    browser.close()
 
 gitSetup(projectName)
 
